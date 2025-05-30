41a2f9c83fa72aec0459a65be4bfa145e6c942ca
Nilesh Jaiswal <nilesh.jaiswal9798@gmail.com>
2025-05-30 09:43:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useSelector } from "react-redux";
+import "./App.css";
+import Container from "./components/Container";
+import Controls from "./components/Controls";
+import DisplayCounter from "./components/DisplayCounter";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PrivacyMsg from "./components/PrivacyMsg";
+
+function App() {
+  const privacy = useSelector((store) => store.privacy);
+
+  return (
+    <center className="px-4 py-5 my-5 text-center">
+      <Container>
+        <Header></Header>
+        <div className="col-lg-6 mx-auto">
+          {privacy ? <PrivacyMsg /> : <DisplayCounter />} <Controls></Controls>
+        </div>
+      </Container>
+    </center>
+  );
+}
+
+export default App;
